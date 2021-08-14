85c45aaa15ac6d9bea61d72fc00eac810fb7d141
meshidenn <mesitahiro@hotmail.com>
2021-08-14 10:57:30 +0900

    add poetry:

--- setup.sh
+++ setup.sh
@@ -3,16 +3,23 @@
 cp -r .config ~/
 cp .tmux.conf ~/
 cp -r .emacs.d ~/
+
+# fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 cp .bashrc ~/
 
+# starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
+# z
 mkdir .commands
 git clone https://github.com/rupa/z.git ~/.commands/z/
 echo ". ~/.commands/z/z.sh" >> ~/.bashrc
 
+# poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+echo "export PATH=$PATH:$HOME/.local/bin"  >> ~/.bashrc
 
 source ~/.bashrc
