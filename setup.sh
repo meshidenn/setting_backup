c0adc62b31a884bec49f52f0f25bc0bc62ab6e64
meshidenn <mesitahiro@hotmail.com>
2021-08-14 09:29:16 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 cp -r .config ~/
-cp .tmux.conf
+cp .tmux.conf ~/
 cp -r .emacs.d ~/
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+cp .bashrc ~/
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-echo 'eval "$(starship init bash)"' >> .bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+mkdir .commands
+git clone https://github.com/rupa/z.git ~/.commands/
+echo "./ ~/.commands/z/z.sh" >> ~/.bashrc
+
+
+source ~/.bashrc
