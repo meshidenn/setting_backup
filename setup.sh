222b822ce9600f22c11370701dec0d7519f33c48
meshidenn <mesitahiro@hotmail.com>
2025-07-29 01:36:04 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
-PATH_TO_SCRIPT=$1
 
-cp -r $PATH_TO_SCRIPT/.config ~/
-cp $PATH_TO_SCRIPT/.tmux.conf ~/
-cp -r $PATH_TO_SCRIPT/.emacs.d ~/
-cp $PATH_TO_SCRIPT/.bashrc ~/
+cp -r .config $HOME
+cp -r .tmux.conf $HOME
+cp -r .bashrc $HOME
 
 # starship
+echo setup starship
 mkdir -p ~/.local/bin
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -b .local/bin
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 # fzf
+echo setup fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # z
+echo setup z
 mkdir .commands
 git clone https://github.com/rupa/z.git ~/.commands/z/
 echo ". ~/.commands/z/z.sh" >> ~/.bashrc
 
 # uv
+echo setup uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 cd $HOME
 
-# starship
-mkdir -p ~/.local/bin
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -b .local/bin
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-
 # kubectl
+echo setup kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check && sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# mise
+echo setup mise
+curl https://mise.run | sh
+~/.local/bin/mise --version
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+mise use --global node
+node -v
+
+# claude
+echo setup claude code
+npm install -g @anthropic-ai/claude-code
+cp -r .claude $HOME
+
+# gemini-cli
+echo setup gemini cli
+npm install -g @google/gemini-cli
 
 # git completion for mac
 ## MAC
